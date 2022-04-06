645bee075a6b2f10db7774044c10047caae9d412
funkyfisch <funkyfisch@gmail.com>
2022-04-06 07:04:49 +0200

    complete material

---
+++ src/customMath.js
@@ -0,0 +1,16 @@
+// add two numbers
+const add = (a, b) => a + b;
+// subtract two numbers
+const subtract = (a, b) => a - b;
+// multiply two numbers
+const multiply = (a, b) => a * b;
+// divide two numbers
+const divide = (a, b) => a / b;
+// square a number
+const square = (a) => a * a;
+// cube a number
+const cube = (a) => a * a * a;
+// raise a number to the power of another number
+const power = (a, b) => Math.pow(a, b);
+
+module.exports = {add, subtract, multiply, divide, square, cube, power};
